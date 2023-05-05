7db537219dfe6861ebe5d9efeea1e7619666ebd7
Alexander Hartelt <hartelt.alexander@gmail.com>
2023-05-05 14:39:25 +0200

    version 0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='PageXML_to_Mask_converter',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     long_description=long_description,
 
